6efbaf1f7a09035d55450b363d58032fd985b46f
Admin <git@webgk.ru>
2016-11-02 14:57:00 +0300

    SUP-5307: Add script insted of phone

--- local/templates/avtox-new/include/telephone.php
+++ local/templates/avtox-new/include/telephone.php
@@ -3,7 +3,9 @@
 <p style="text-align: center;">
  <span style="font-size: 10pt;">
 	Многоканальный телефон</span><br>
- <b><span style="font-size: 14pt;">+7 (495) 150-12-95</span></b>
+ <b><span style="font-size: 14pt;">
+ 	<script type="text/javascript" src="//widget.mango-office.ru/js/c-t-w.min.js"></script><div class="m-call-tracking-widget" data-settings='{"id":1818}'></div><script type="application/javascript">!function(t,e){for(var i=document.getElementsByClassName(t),n=0;n<i.length;n++){var r=i[n];if("true"!=r.getAttribute("init")){var a=new MCTWidget({host:e,elem:r});a.initWidget(),r.setAttribute("init","true")}}}("m-call-tracking-widget", window.location.protocol + "//widget.mango-office.ru");</script>
+ </span></b>
 </p>
  <span style="font-size: 14pt;"> </span>
 <p style="text-align: center;">
